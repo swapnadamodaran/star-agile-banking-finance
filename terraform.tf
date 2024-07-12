747029cff05b2f1a794b3b704ac049603b0630cb
swapnadamodaran <swapnaandsun@gmail.com>
2024-07-12 02:47:13 -0400

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,3 @@
-#Initialize Terraform
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
 
 # Select region
 provider "aws" {
